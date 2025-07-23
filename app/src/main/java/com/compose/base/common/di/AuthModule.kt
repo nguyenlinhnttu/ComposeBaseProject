a82b8c3c12028daa92dfa8339ba92331be05da74
Nguyen Linh <linhvan@bravesoft.com.vn>
2025-07-23 12:12:26 +0700

    Revert "clean: update hilt inject, make ApiService for all api"
    This reverts commit cbdbe7cf83b23846584dde4a71902a1533da0318.

--- app/src/main/java/com/compose/base/common/di/AuthModule.kt
+++ app/src/main/java/com/compose/base/common/di/AuthModule.kt
@@ -1,17 +1,27 @@
 package com.compose.base.common.di
 
+import com.compose.base.data.remote.AuthApiService
 import com.compose.base.data.repository.AuthRepository
 import com.compose.base.data.repository.AuthRepositoryImpl
-import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
 import dagger.hilt.android.scopes.ViewModelScoped
+import retrofit2.Retrofit
 
 @Module
 @InstallIn(ViewModelComponent::class) // Or SingletonComponent if you want AuthRepository to be app-wide
-abstract class RepositoryModule {
-    @Binds
+object AuthModule {
+    @Provides
+    @ViewModelScoped // Scope AuthApiService to the ViewModel lifetime
+    fun provideAuthApiService(retrofit: Retrofit): AuthApiService {
+        return retrofit.create(AuthApiService::class.java)
+    }
+
+    @Provides
     @ViewModelScoped // Scope AuthRepository to the ViewModel lifetime
-    abstract fun bindAuthRepository(impl: AuthRepositoryImpl): AuthRepository
+    fun provideAuthRepository(apiService: AuthApiService): AuthRepository {
+        return AuthRepositoryImpl(apiService)
+    }
 }
--- app/src/main/java/com/compose/base/common/di/NetworkModule.kt
+++ app/src/main/java/com/compose/base/common/di/NetworkModule.kt
@@ -1,12 +1,13 @@
 package com.compose.base.common.di
 
+import android.content.Context
 import com.compose.base.BuildConfig
 import com.compose.base.common.constants.Constants
-import com.compose.base.data.remote.ApiService
 import com.google.gson.Gson
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -32,6 +33,7 @@ object NetworkModule {
     fun provideOkHttpClient(
         httpLoggingInterceptor: HttpLoggingInterceptor,
         applicationInterceptor: AuthorizationInterceptor,
+        @ApplicationContext context: Context,
     ): OkHttpClient {
         val client = OkHttpClient.Builder()
             .addInterceptor(applicationInterceptor)
@@ -50,16 +52,10 @@ object NetworkModule {
     @Singleton
     fun provideRetrofit(okHttpClient: OkHttpClient, gson: Gson): Retrofit {
         return Retrofit.Builder()
-            .baseUrl("https://dev-api.domico.jp/api/")
+            .baseUrl(BuildConfig.HOST_URL)
             .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create(gson))
             .build()
-    }
-
-    @Provides
-    @Singleton
-    fun provideApiService(retrofit: Retrofit): ApiService {
-        return retrofit.create(ApiService::class.java)
     }
 
     // No API service provided here yet, as each feature will provide its own.
